89a26a963fd1d6e3fbd3e389797c775bcd45469b
AnleAnja <anleanja@live.de>
2020-09-10 15:02:50 +0200

    fix minify js

--- Frontend/dev/assets/js/main.js
+++ Frontend/dev/assets/js/main.js
@@ -1,3 +1,7 @@
+// regeneratorRuntime is needed for webpack, specifically for async / await
+// eslint-disable-next-line no-unused-vars
+import regeneratorRuntime from 'regenerator-runtime';
+
 console.log('Hello World');
 // eslint-disable-next-line no-undef
 alert('Hallo Welt');
