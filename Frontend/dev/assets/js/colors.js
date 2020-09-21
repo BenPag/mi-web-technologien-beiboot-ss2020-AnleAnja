ad38f0030e78e3704eb7c33e4b9c78e4f816abed
BEP <bpagelsdorf@outlook.de>
2020-09-21 22:39:26 +0200

    change styling of quote

--- Frontend/dev/assets/js/colors.js
+++ Frontend/dev/assets/js/colors.js
@@ -21,16 +21,16 @@ function invertColor(hex, luminance) {
         b = darken(255 - parseInt(hex.slice(4, 6), 16));
     }
 
-    return `rgba(${r},${g},${b},0.8)`;
+    return `rgb(${r},${g},${b})`;
 }
 
 function lighten(component) {
-    const result = Math.ceil(component * 1.5);
+    const result = Math.ceil(component * 1.6);
     return result > 255 ? 255 : result;
 }
 
 function darken(component) {
-    return Math.floor(component * 0.5);
+    return Math.floor(component * 0.6);
 }
 
 export default {
@@ -39,5 +39,18 @@ export default {
     },
     getRgbaFromRgb: ({rgb}, alpha = 0.75) => {
         return `rgba(${rgb.join(',')},${alpha})`;
+    },
+    getGradientOfColor(colors) {
+        return `
+        radial-gradient(circle at center, ${this.getRgbaFromRgb(colors[0], 0.8)} 35%, transparent 67%),
+        radial-gradient(circle at 25% 25%, ${this.getRgbaFromRgb(colors[2], 0.55)} 10%, transparent 25%),
+        radial-gradient(circle at 25% 75%, ${this.getRgbaFromRgb(colors[4], 0.55)} 10%, transparent 25%),
+        radial-gradient(circle at 75% 25%, ${this.getRgbaFromRgb(colors[3], 0.55)} 10%, transparent 25%),
+        radial-gradient(circle at 75% 75%, ${this.getRgbaFromRgb(colors[1], 0.55)} 10%, transparent 25%)`;
+        //return `linear-gradient(${color1.color} 0%, ${color2.color} 100%)`;
+        //return `radial-gradient(rgba(${color1.rgb.join(',')}, 0.8), rgba(${color2.rgb.join(',')}, 0.8))`;
+        //return `radial-gradient(ellipse at top, ${color1.color}, transparent), radial-gradient(ellipse at bottom, ${color2.color}, transparent)`;
+        //return `radial-gradient(circle, rgba(${rgbStr1},1) 0%, rgba(${rgbStr1},0.8) 50%, rgba(${rgbStr2},0.8) 50%, rgba(${rgbStr2},0.1) 95%, rgba(${rgbStr2},0) 100%)`;
+        //linear-gradient(90deg, transparent 0%, rgba(${rgbStr},0.5) 10%, rgba(${rgbStr},0.8) 50%, rgba(${rgbStr},0.5) 90%, transparent 100%),
     }
 };
--- Frontend/dev/assets/js/htmlBuilder.js
+++ Frontend/dev/assets/js/htmlBuilder.js
@@ -5,25 +5,29 @@ function getCardBackground({path}) {
 }
 
 function getCardTextBox({date, quote, author}) {
-    const dateText = document.createElement('strong');
-    dateText.className = 'date';
+    const dateText = document.createElement('span');
     dateText.innerText = date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+
+    const dateTextBox = document.createElement('div');
+    dateTextBox.className = 'date';
+    dateTextBox.append(dateText);
 
     const text = document.createElement('p');
     text.innerText = quote;
 
-    const authorText = document.createElement('strong');
-    authorText.className = 'author';
+    const authorText = document.createElement('span');
     authorText.innerText = author;
+
+    const authorTextBox = document.createElement('div');
+    authorTextBox.className = 'author';
+    authorTextBox.append(authorText);
 
     const textBox = document.createElement('div');
     textBox.className = 'quote--text-box';
-    textBox.append(dateText, text, authorText);
+    textBox.append(dateTextBox, text, authorTextBox);
 
     return textBox;
 }
-
-
 
 export default {
     getHtmlCardOfQuote(quote) {
@@ -37,6 +41,7 @@ export default {
         const colorBox = document.createElement('div');
         colorBox.className = 'quote--color-box';
         colorBox.style.backgroundColor = colors.getRgbaFromRgb(quote.background.hexcodes[0]);
+        colorBox.style.background = colors.getGradientOfColor(quote.background.hexcodes);
         colorBox.style.color = colors.getTextColor(quote.background.hexcodes[0]);
 
         colorBox.append(getCardTextBox(quote));
